3367c80ff1383488d225796c04a823f9cbed6069
Daniel <danielmccormick99@gmail.com>
2019-02-16 14:34:30 -0800

    updated data pattern

--- data_stripper.py
+++ data_stripper.py
@@ -1,16 +1,42 @@
 import csv
+import re
+import time
+import datetime
+
+def to_dt(inputTime): 
+    ret = int(inputTime[0:1])*3600 + int(inputTime[3:4])*60
+    return str(ret)
+
+def coordinates(inputCoord): 
+    arbitrary = inputCoord[1:-1]
+    return arbitrary
+
+def to_severity(severity):
+    #NON-CRIMINAL ROBBERY,  ASSAULT, VANDALISM, SECONDARY CODES, BURGLARY, LARCENY/THEFT, DRUG/NARCOTIC
+    #WARRANTS VEHICLE THEFT ROBBERY
+    if (severity == "ROBBERY"):
+        return str(4)
+    elif (severity == "ASSAULT"):
+        return str(5)
+    elif (severity == "VANDALISM" or severity == "BURGARLY" or severity == "VEHICLE THEFT" or severity == "ROBBERY"):
+        return str(3)
+    elif (severity == "SECONDARY CODES" or severity == "LARCENY/THEFT" or severity == "DRUG/NARCOTIC"):
+        return str(2)
+    elif (severity == "WARRANTS"):
+        return str(2)
+    else:
+        return str(1)
 
 if __name__ == '__main__':
     fp = input("What is the input path? ")
     op = input("What is the output path? ")
-    output_file = open(op, 'w')
-    output_file.writelines("Date,Location\n")
+    #output_file = open(op, 'w')
+    #output_file.writelines("Severity,Date,Location\n")
     with open(fp, newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            output_file.write(row['Date'] + " " + row['Location'] + "\n")
+        with open(op, 'a') as output_file:
+            reader = csv.DictReader(csvfile)    
+            for row in reader:
+                if (row['Date'].endswith("2017")):
+                    output_file.write(to_dt(row['Time']) + "," + coordinates(row['Location']) + ',' + to_severity(row['Category']) + "\n")
 
-    output_file.close()
-
-
-    
+   
