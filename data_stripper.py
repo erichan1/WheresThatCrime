b51b63f8471fd71fb2182aadbbe0b561ebb6b21e
Daniel <danielmccormick99@gmail.com>
2019-02-17 03:22:02 -0800

    updated comments

--- data_stripper.py
+++ data_stripper.py
@@ -50,29 +50,39 @@ def reint_day(day):
            return "WARNING! INVALID ARG PASSED TO REINT_DAY"
 
 def retrieve_inputs ():
+    # This will get the first 3 rows, or the 'X' input
     my_data = np.genfromtxt('current_stat.csv', delimiter=',', skip_header=0, usecols = (0, 1, 2), replace_space='')
     #print(my_data)
     return my_data
 
 def retrieve_outputs (): 
+    # This will get the 4th row, the 'Y' output
     my_data = np.genfromtxt('current_stat.csv', delimiter=',', skip_head=0, usecols = (3), replace_space='')
     return my_data
 
 def retrieve_test(): 
+    # Loads into numpy array
     my_data = np.genfromtxt('current_model.csv', delimiter=',', skip_head=0, replace_space='')
     return my_data
 
+
+# Handles getting data
+#
+# @ param mo is the month for filtering
+# @ param yr is the year for filtering
+# @param rl is the bool for actually getting data
 def get_data(mo, yr, rl): #rl would be really copying, if false it'll instead install 
+    # inp = input. Node: requires SF_Crime_Heat_Map.csv outside of dir
     inp = "../SF_Crime_Heat_Map.csv"
     if (rl == True):
-        otp = "models/current_stat.csv"   
+        otp = "models/current_stat.csv"  # dual define
     else: 
         otp = "models/current_model.csv"
-    with open(inp, newline='') as csvfile:
-        with open(otp, 'a') as output_file:
-            reader = csv.DictReader(csvfile)
+    with open(inp, newline='') as csvfile: # with open means no need to manually close the file
+        with open(otp, 'a') as output_file: # nested with open because read and write access
+            reader = csv.DictReader(csvfile) #CVS reader to parse data
             for row in reader:
-                if (row['Date'] and row['Date'].endswith(yr) and row['Date'].startswith(mo)):
+                if (row['Date'] and row['Date'].endswith(yr) and row['Date'].startswith(mo)): # Validates the data
                     if (rl):
                         output_file.write(reint_day(row['DayOfWeek']) + "," + coordinates(row['Location']) + ',' + to_severity(row['Category']) + "\n")
                     else: 
